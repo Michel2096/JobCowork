961565356cedfe79e7c90c42e0842539ddafadc5
Brendagh6 <bh4331265@gmail.com>
2025-04-09 09:21:50 -0600

    app

--- src/components/UbicacionesEdit.jsx
+++ src/components/UbicacionesEdit.jsx
@@ -70,6 +70,7 @@ const UbicacionesEdit = () => {
                             type="text"
                             id="nombre"
                             name="nombre"
+                            placeholder="Nombre"
                             value={ubicacion.nombre}
                             onChange={handleChange}
                             className="form-control"
@@ -81,6 +82,7 @@ const UbicacionesEdit = () => {
                             type="text"
                             id="tipo"
                             name="tipo"
+                            placeholder="Tipo"
                             value={ubicacion.tipo}
                             onChange={handleChange}
                             className="form-control"
